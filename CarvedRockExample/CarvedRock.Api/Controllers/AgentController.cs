34b16d2dce7e1bed1d65cb7b17027d5ec6fdcfd6
Geert Huls <geert.huls82@gmail.com>
2026-05-11 23:22:29 +0200

    Call locally hosted LLM with MAF

---
+++ CarvedRockExample/CarvedRock.Api/Controllers/AgentController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Agents.AI;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using OllamaSharp;
+using System.Runtime.CompilerServices;
+
+namespace CarvedRock.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class AgentController : ControllerBase
+{
+    [AllowAnonymous]
+    [HttpGet]
+    public async IAsyncEnumerable<string> Get([EnumeratorCancellation] CancellationToken cxl)
+    {
+        var chatClient = new OllamaApiClient(new Uri("http://localhost:11434"), "ministral-3");
+        var agent = new ChatClientAgent(chatClient, instructions: "You are good at telling jokes.", name: "Joker");
+
+        var session = await agent.CreateSessionAsync(cxl);
+
+        await foreach (var update in agent.RunStreamingAsync("Tell me a joke about Alice in Wonderland.",
+            session, cancellationToken: cxl))
+        {
+            yield return update.ToString();
+        };
+    }
+}
